bd0d6b9cc19cabb59c7a50a16f907fcf81ed7737
laurenx <santillanmitchlauren@gmail.com>
2024-06-30 02:32:00 +0800

    [FIXED][MITCH] conflicts

--- config.php
+++ config.php
@@ -4,5 +4,5 @@
     $conn = mysqli_connect("localhost","root","","accounts") or die("Couldn't connect");
     // $conn = mysqli_connect('sql312.infinityfree.com', 'if0_36811532', 'EOAadDVdofVT', 'if0_36811532_teknoDB')or die("Couldn't connect");
     
-
+    
 ?>
