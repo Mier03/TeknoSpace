31eb44f769d473deb439d3c0ce7472f5cca0c85f
Coder.Brian <mierangelina47@gmail.com>
2024-07-02 16:54:49 +0800

    fix: finalize student & faculty comments

--- SUNGAHID/create_post.php
+++ SUNGAHID/create_post.php
@@ -13,7 +13,7 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $content = $_POST['content'];
     $audience = $_POST['audience'];
-    $username = $_SESSION['username']; 
+    $username = $_SESSION['firstName'].' '. $_SESSION['lastName']; // Replace with dynamic user name if applicable
     $profile_image = "https://static.thenounproject.com/png/3918329-200.png"; // Replace with dynamic profile image if applicable
     $imagePath = "";
 
