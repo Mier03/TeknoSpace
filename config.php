646dc759c6509f9cf9ea1d99a0afdba265d73814
Mier <mierangelina47@gmail.com>
2024-07-22 10:40:53 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,18 @@
 <?php 
+    $env = $_SERVER['SERVER_NAME'];
+    $dbHost = 'localhost';
+    $dbUser = 'root';
+    $dbPass = '';
+    $dbName = 'teknospace';
 
+    if ($env != 'localhost') {
+        $dbHost = 'localhost';
+        $dbUser = 'angie';
+        $dbPass = 'angiedb234;;';
+        $dbName = 'pljrluqz_teknospace';
+    }
  
-    $conn = mysqli_connect("localhost","root","","teknospace") or die("Couldn't connect");
+    $conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName) or die("Couldn't connect");
    // $conn = mysqli_connect('sql312.infinityfree.com', 'if0_36811532', 'EOAadDVdofVT', 'if0_36811532_teknoDB')or die("Couldn't connect");
     return $conn;
 ?>
