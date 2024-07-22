9e0a0d4e17f76159da37b75b370ca4902e92a643
Adrianne-John <adriannejohncamusofficial@gmail.com>
2024-07-22 14:08:10 +0800

    [Fix][CAMUS] index.php styles
    - Fixed index.php styles to link to aboutus.css

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (isset($_POST['submit_signup'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us</title>
     <!-- HERE -->
-    <link rel="stylesheet" href="welcomeaboutus.css">
+    <link rel="stylesheet" href="welcome/aboutus.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
