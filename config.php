cafd00b353426e2c737fa600185d8cee45f5c60d
Mier <mierangelina47@gmail.com>
2024-07-23 20:27:35 +0800

    fix:connection for deployment

--- config.php
+++ config.php
@@ -1,5 +1,4 @@
 <?php 
-/*
     $env = $_SERVER['SERVER_NAME'];
     $dbHost = 'localhost';
     $dbUser = 'root';
@@ -18,8 +17,8 @@
     } catch (Exception $e) {
         echo "Error: " . $e->getMessage();
     }
-*/
-    $conn = mysqli_connect('localhost', 'root', '', 'teknospace')or die("Couldn't connect");
+
+    // $conn = mysqli_connect('localhost', 'root', '', 'teknospace')or die("Couldn't connect");
    // $conn = mysqli_connect('sql312.infinityfree.com', 'if0_36811532', 'EOAadDVdofVT', 'if0_36811532_teknoDB')or die("Couldn't connect");
     return $conn;
 ?>
